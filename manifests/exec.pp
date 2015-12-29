19c18ca17a8cc72d3553c37f0b6ddf1bb4af3b18
Matthew Hope <mhope@adaptavist.com>
2015-12-29 16:23:16 +0000

    lint fixes

--- manifests/exec.pp
+++ manifests/exec.pp
@@ -1,19 +1,19 @@
 
 # 
-# $docker_url  = "false",
+# $docker_url  = 'false',
 # Defines url to download tarball from, see docker::image docker_tar param documentation
 # 
-# $docker_url_tarball_name = "false"
+# $docker_url_tarball_name = 'false'
 # Downloaded tarball name to be passed as docker_tar to docker::image, TODO: parse it from url
 # 
 
 define docker_config::exec(
-    $detach = "false",
-    $interactive = "false",
-    $tty = "false",
-    $container = "false",
-    $command = "false",
-    $sanitise_name = "true",
+    $detach = 'false',
+    $interactive = 'false',
+    $tty = 'false',
+    $container = 'false',
+    $command = 'false',
+    $sanitise_name = 'true',
 ){
     include docker_config
 
@@ -22,24 +22,24 @@ define docker_config::exec(
     $real_tty = str2bool($tty)
     $real_sanitise_name = str2bool($sanitise_name)
 
-    if ($container == "false"){
+    if ($container == 'false'){
         $real_container = undef
     } else {
         $real_container = $container
     }
 
-    if ($command == "false"){
+    if ($command == 'false'){
         $real_command = undef
     } else {
         $real_command = $command
     }
     
-    docker::exec{ "$name":
-        detach => $real_detach,
-        interactive => $real_interactive,
-        tty => $real_tty,
-        container => $real_container,
-        command => $real_command,
+    docker::exec{ $name:
+        detach        => $real_detach,
+        interactive   => $real_interactive,
+        tty           => $real_tty,
+        container     => $real_container,
+        command       => $real_command,
         sanitise_name => $real_sanitise_name,
     }
 
--- manifests/run.pp
+++ manifests/run.pp
@@ -2,121 +2,121 @@
 # docker_config::run
 # 
 
-# $detach: value "default" will be changed to undef, "true" => true and "false" => false, see docker::run for details
+# $detach: value "default" will be changed to undef, "true" => true and 'false' => false, see docker::run for details
 
 define docker_config::run(
     $image,
-    $command = "false",
+    $command = 'false',
     $memory_limit = '0b',
-    $cpuset = "",
-    $ports = "",
-    $expose = "",
-    $volumes = "",
-    $links = "",
-    $use_name = "false",
-    $running = "true",
-    $volumes_from = "",
+    $cpuset = '',
+    $ports = '',
+    $expose = '',
+    $volumes = '',
+    $links = '',
+    $use_name = 'false',
+    $running = 'true',
+    $volumes_from = '',
     $net = 'bridge',
-    $username = "false",
-    $hostname = "false",
-    $env = "",
-    $env_file = "",
-    $dns = "",
-    $dns_search = "",
-    $lxc_conf = "",
-    $restart_service = "true",
-    $disable_network = "false",
-    $privileged = "false",
-    $detach = "default",
-    $extra_parameters = "false",
-    $pull_on_start = "false",
-    $depends = "",
-    $tty = "false",
-    $socket_connect = "",
-    $hostentries = "",
-    $restart = "false",
-){     
+    $username = 'false',
+    $hostname = 'false',
+    $env = '',
+    $env_file = '',
+    $dns = '',
+    $dns_search = '',
+    $lxc_conf = '',
+    $restart_service = 'true',
+    $disable_network = 'false',
+    $privileged = 'false',
+    $detach = 'default',
+    $extra_parameters = 'false',
+    $pull_on_start = 'false',
+    $depends = '',
+    $tty = 'false',
+    $socket_connect = '',
+    $hostentries = '',
+    $restart = 'false',
+){
     include docker_config
     
-    if ($hostentries == ""){
+    if ($hostentries == ''){
         $real_hostentries = []
     } else {
         $real_hostentries = $hostentries
     }
 
-    if ($socket_connect == ""){
+    if ($socket_connect == ''){
         $real_socket_connect = []
     } else {
         $real_socket_connect = $socket_connect
     }
 
-    if ($depends == ""){
+    if ($depends == ''){
         $real_depends = []
     } else {
         $real_depends = $depends
     }
 
-    if ($lxc_conf == ""){
+    if ($lxc_conf == ''){
         $real_lxc_conf = []
     } else {
         $real_lxc_conf = $lxc_conf
     }
 
-    if ($dns_search == ""){
+    if ($dns_search == ''){
         $real_dns_search = []
     } else {
         $real_dns_search = $dns_search
     }
 
-    if ($dns == ""){
+    if ($dns == ''){
         $real_dns = []
     } else {
         $real_dns = $dns
     }
 
-    if ($env_file == ""){
+    if ($env_file == ''){
         $real_env_file = []
     } else {
         $real_env_file = $env_file
     }
 
-    if ($env == ""){
+    if ($env == ''){
         $real_env = []
     } else {
         $real_env = $env
     }
 
-    if ($volumes_from == ""){
+    if ($volumes_from == ''){
         $real_volumes_from = []
     } else {
         $real_volumes_from = $volumes_from
     }
 
-    if ($links == ""){
+    if ($links == ''){
         $real_links = []
     } else {
         $real_links = $links
     }
     
-    if ($volumes == ""){
+    if ($volumes == ''){
         $real_volumes = []
     } else {
         $real_volumes = $volumes
     }
 
-    if ($expose == ""){
+    if ($expose == ''){
         $real_expose = []
     } else {
         $real_expose = $expose
     }
 
-    if ($ports == ""){
+    if ($ports == ''){
         $real_ports = []
     } else {
         $real_ports = $ports
     }
 
-    if ($cpuset == ""){
+    if ($cpuset == ''){
         $real_cpuset = []
     } else {
         $real_cpuset = $cpuset
@@ -128,13 +128,13 @@ define docker_config::run(
     $real_restart_service = str2bool($restart_service)
     $real_disable_network = str2bool($disable_network)
     
-    if ($hostname == "false"){
+    if ($hostname == 'false'){
         $real_hostname = false
     } else {
         $real_hostname = $hostname
     }
     
-    if ($username == "false"){
+    if ($username == 'false'){
         $real_username = false
     } else {
         $real_username = $username
@@ -143,60 +143,60 @@ define docker_config::run(
     $real_running = str2bool($running)
     $real_use_name = str2bool($use_name)
 
-    if ($restart == "false"){
+    if ($restart == 'false'){
         $real_restart = undef
     } else {
         $real_restart = $restart
     }
 
-    if ($extra_parameters == "false"){
+    if ($extra_parameters == 'false'){
         $real_extra_parameters = undef
     } else {
         $real_extra_parameters = $extra_parameters
     }
 
-    if ($command == "false"){
+    if ($command == 'false'){
         $real_command = undef
     } else {
         $real_command = $command
     }
 
-    if ($detach == "default"){
+    if ($detach == 'default'){
         $real_detach = undef
     } else {
         $real_detach = str2bool($detach)
     }
 
-    docker::run{ "$name":
-        image   => $image,
-        command => $real_command,
-        memory_limit => $memory_limit,
-        cpuset => $real_cpuset,
-        ports => $real_ports,
-        expose => $real_expose,
-        volumes => $real_volumes,
-        links => $real_links,
-        use_name => $real_use_name,
-        running => $real_running,
-        volumes_from => $real_volumes_from,
-        net => $net,
-        username => $real_username,
-        hostname => $real_hostname,
-        env => $real_env,
-        env_file => $real_env_file,
-        dns => $real_dns,
-        dns_search => $real_dns_search,
-        lxc_conf => $real_lxc_conf,
-        restart_service => $real_restart_service,
-        disable_network => $real_disable_network,
-        privileged => $real_privileged,
-        detach => $real_detach,
+    docker::run{ $name:
+        image            => $image,
+        command          => $real_command,
+        memory_limit     => $memory_limit,
+        cpuset           => $real_cpuset,
+        ports            => $real_ports,
+        expose           => $real_expose,
+        volumes          => $real_volumes,
+        links            => $real_links,
+        use_name         => $real_use_name,
+        running          => $real_running,
+        volumes_from     => $real_volumes_from,
+        net              => $net,
+        username         => $real_username,
+        hostname         => $real_hostname,
+        env              => $real_env,
+        env_file         => $real_env_file,
+        dns              => $real_dns,
+        dns_search       => $real_dns_search,
+        lxc_conf         => $real_lxc_conf,
+        restart_service  => $real_restart_service,
+        disable_network  => $real_disable_network,
+        privileged       => $real_privileged,
+        detach           => $real_detach,
         extra_parameters => $real_extra_parameters,
-        pull_on_start => $real_pull_on_start,
-        depends => $real_depends,
-        tty => $real_tty,
-        socket_connect => $real_socket_connect,
-        hostentries => $real_hostentries,
-        restart => $real_restart,
+        pull_on_start    => $real_pull_on_start,
+        depends          => $real_depends,
+        tty              => $real_tty,
+        socket_connect   => $real_socket_connect,
+        hostentries      => $real_hostentries,
+        restart          => $real_restart,
     }
 }
